df75c1ec8fe6c660c55f86942efc7f4474c726fb
OmarMartinez0637 <a.23308051280637@cbtis128.edu.mx>
2025-10-06 12:07:48 -0600

    first commit

---
+++ app_menu/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# clase Menu con los campos nombre, precio y descripcion
+class Menu(models.Model):
+    nombre= models.CharField(max_length=100)
+    precio= models.FloatField()
+    descripcion= models.CharField(max_length=100)
